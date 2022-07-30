d2c908ee04ce810d7ff670c84580acb2ac28bbbf
kenyiherryalcantaraastete <kalcantaraa@uni.pe>
2022-07-29 23:31:48 -0500

    Configuracion inicial de app

--- src/main/java/Container.java
+++ src/main/java/Container.java
@@ -3,13 +3,13 @@ import java.util.ArrayList;
 
 
 
-public class Contenedor {
+public class Container {
     private ArrayList<Item> items = new ArrayList<>();
     private int costoTotal;
     private int pesoTotal;
-    public Contenedor(){
+    public Container(){
 
     }
 
-    class
+
 }
---
+++ src/main/java/GUIContainer.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class GUIContainer extends JFrame {
+    private JTextField textField1;
+    private JButton addButton;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JPanel PanelInputItem;
+    private JPanel PanelShowItem;
+    private JPanel PanelContainer;
+    private JButton FinishedButton;
+
+
+    public GUIContainer(){
+        setContentPane(PanelContainer);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(250,100,720,520);
+        setBounds(250,100,820,620);
+        setVisible(true);
+    }
+}
