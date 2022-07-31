25b32a9076416d90f16c1ff5ad11aac54d4c620c
kenyiherryalcantaraastete <kalcantaraa@uni.pe>
2022-07-30 23:10:18 -0500

    a

--- src/main/java/view/GUIApp.java
+++ src/main/java/view/GUIApp.java
@@ -6,12 +6,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GUIApp extends JFrame {
-    private JButton enviarPorTierraButton;
+    private JButton enviar;
     private JButton enlistarItemsButton;
     private JPanel PanelAppPrincipal;
     private JLabel mensajeDeTipoDeEnvio;
     private JPanel panelMensaje;
     private JPanel topPanel;
+    private JPanel mainPanel;
 
     private GUIContainer guiContainer;
 
@@ -29,11 +30,13 @@ public class GUIApp extends JFrame {
 
     public GUIApp(){
 
+
         PanelAppPrincipal.setLayout(new BorderLayout());
         PanelAppPrincipal.add(topPanel,BorderLayout.NORTH);
-        PanelAppPrincipal.add(PanelAppPrincipal,BorderLayout.CENTER);
+        PanelAppPrincipal.add(mainPanel,BorderLayout.CENTER);
         PanelAppPrincipal.add(panelMensaje,BorderLayout.SOUTH);
 
+        setContentPane( PanelAppPrincipal);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(250,100,720,520);
         setBounds(250,100,820,620);
@@ -44,7 +47,7 @@ public class GUIApp extends JFrame {
                 guiContainer = new GUIContainer();
             }
         });
-        enviarPorTierraButton.addActionListener(new ActionListener() {
+        enviar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
