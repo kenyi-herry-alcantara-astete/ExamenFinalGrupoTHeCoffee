6fa8b1a3484508fa0b3878063de26836fc8143e2
kenyiherryalcantaraastete <kalcantaraa@uni.pe>
2022-07-30 20:23:23 -0500

    Primera estructura de mvc

---
+++ src/main/java/view/GUIApp.java
@@ -0,0 +1,33 @@
+package view;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUIApp extends JFrame {
+    private JButton enviarPorTierraButton;
+    private JButton enviarPorAireButton;
+    private JButton enlistarItemsButton;
+    private JPanel PanelAppPrincipal;
+
+    private GUIContainer guiContainer;
+
+    public static void main(String[] args) {
+        GUIApp guiApp = new GUIApp();
+    }
+
+    public GUIApp(){
+
+        setContentPane( PanelAppPrincipal);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(250,100,720,520);
+        setBounds(250,100,820,620);
+        setVisible(true);
+        enlistarItemsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                guiContainer = new GUIContainer();
+            }
+        });
+    }
+}
